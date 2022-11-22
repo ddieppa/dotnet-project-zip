b65012a0755979aa069d3239da782c6af487e827
Daniel Dieppa <daniel.dieppa@servicechannel.com>
2022-11-22 16:54:31 -0500

    First commit

---
+++ gitzipConsole/Program.cs
@@ -0,0 +1,35 @@
+﻿using CliWrap;
+using Spectre.Console;
+
+
+
+var currDir = AnsiConsole.Ask<string>("Enter [green]directory[/] to list files in:");
+
+IEnumerable<string> directoriesToIgnore = new List<string> { "node_modules", "bin", "obj", ".idea" };
+var childDirectories = Directory.GetDirectories(currDir);
+var directories = childDirectories.Where(x => !directoriesToIgnore.Any(x.Contains));
+
+
+foreach (var directory in directories)
+{
+    AnsiConsole.MarkupLine($"[green]{directory}[/]");
+    await GitArchiveDirectory(directory);
+}
+
+async Task GitArchiveDirectory(string directory)
+{
+    var gitArchiveCmd = Cli.Wrap("git")
+        .WithWorkingDirectory(directory)
+        .WithArguments(args => args
+            .Add("archive")
+            .Add("HEAD")
+            .Add("--format=zip")
+            .Add("-o")
+            .Add($"{directory}.zip")
+        );
+    AnsiConsole.MarkupLine($"[yellow]{gitArchiveCmd}[/]");
+    
+    await gitArchiveCmd.ExecuteAsync();
+
+}
+
