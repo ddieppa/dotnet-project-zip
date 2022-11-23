ec06737eedd75d10a3c877a9937fa0d349582516
Daniel Dieppa <daniel.dieppa@servicechannel.com>
2022-11-22 19:29:12 -0500

    Add progress

--- gitzipConsole/Program.cs
+++ gitzipConsole/Program.cs
@@ -3,25 +3,38 @@ using Spectre.Console;
 
 
 
-var currDir = AnsiConsole.Ask<string>("Enter [green]directory[/] to list files in:");
-
+//var currDir = AnsiConsole.Ask<string>("Enter [green]directory[/] to list files in:");
+var currDir = $"C:\\servicechannel\\dev";
 IEnumerable<string> directoriesToIgnore = new List<string> { "node_modules", "bin", "obj", ".idea" };
 var childDirectories = Directory.GetDirectories(currDir);
 var directories = childDirectories.Where(x => !directoriesToIgnore.Any(x.Contains));
 
+// Progress
+await AnsiConsole.Progress()
+    .Columns(new TaskDescriptionColumn(), new ProgressBarColumn(), new SpinnerColumn())
+    .StartAsync(async ctx =>
+    {
+        await Task.WhenAll(directories.Select(async directoryName =>
+        {
+            var task = ctx.AddTask(directoryName, new ProgressTaskSettings
+            {
+                AutoStart = false
+            });
 
-foreach (var directory in directories)
-{
-    AnsiConsole.MarkupLine($"[green]{directory}[/]");
-    await GitArchiveDirectory(directory);
-}
+            await GitArchiveDirectory(task, directoryName);
+        }));
+    });
+AnsiConsole.MarkupLine("Done!");
 
-async Task GitArchiveDirectory(string directory)
+
+async Task GitArchiveDirectory(ProgressTask task, string directory)
 {
     try
     {
+        task.StartTask();
         var gitArchiveCmd = Cli.Wrap("git")
             .WithWorkingDirectory(directory)
+            .WithValidation(CommandResultValidation.None)
             .WithArguments(args => args
                 .Add("archive")
                 .Add("HEAD")
@@ -29,14 +42,19 @@ async Task GitArchiveDirectory(string directory)
                 .Add("-o")
                 .Add($"{directory}.zip")
             );
-        AnsiConsole.MarkupLine($"[yellow]{gitArchiveCmd}[/]");
-    
         await gitArchiveCmd.ExecuteAsync();
+        AnsiConsole.MarkupLine($"[green]Archived[/] {directory}");
+        
     }
     catch (Exception e)
     {
-       AnsiConsole.MarkupLine($"[red]{e.Message}[/]");
+       AnsiConsole.MarkupLine($"[red]Error: {e.Message}[/]");
     }
 
+}
+
+void WriteLogMessage(string message)
+{
+    AnsiConsole.MarkupLine($"[grey]LOG:[/] {message}[grey]...[/]");
 }
 
